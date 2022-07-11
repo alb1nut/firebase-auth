8b6c2026de9113b9747e8105529ce862dec9d17a
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-11 04:12:44 +0000

    creted signin function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,17 +1,27 @@
 import { useContext, createContext } from "react";
 
+import { auth } from "./../firebase";
+import {
+  GoogleAuthProvider,
+  signInWithpopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithpopup(auth, provider);
+  };
   return (
-    <AuthContext.AuthContextProvider
-      value={{}}
-    >
-        {children}
+    <AuthContext.AuthContextProvider value={{googleSignIn}}>
+      {children}
     </AuthContext.AuthContextProvider>
   );
 };
 
-export const UserAuth =()=>{
-    return useContext(AuthContext)
-}
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
