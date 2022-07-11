352c81b56e7c2e25d46d1032b170cdea739beb1e
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-11 00:26:37 +0000

    added account page component

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Account = () => {
   return (
-    <div>
-      
+    <div className="w-[300] m-auto">
+      <h1 className="text-center text-2xl font-bold pt-12">Account</h1>
+      <div>
+        <p>Welcome, User</p>
+      </div>
+      <button className="border py-2 px-5 mt-10">Logout</button>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
