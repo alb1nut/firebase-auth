b5b18dcd342c5e8f78541cb3f58f8287c6fd978e
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-11 00:23:57 +0000

    added home page component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Account from "./pages/Account";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </div>
   );
 }
