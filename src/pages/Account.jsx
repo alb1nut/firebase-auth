c16c57faecd0467d0ecfbc0e89180adb28702179
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-12 18:15:38 +0000

    created protected rout function

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { UserAuth } from "../context/AuthContext";
 
-const Account = () => {
 
-  const { user } =UserAuth()
-  console.log(user);
+const Account = () => {
+  const navigate = useNavigate()
+  const { user,logOut } =UserAuth()
+  const handleSignOut = async() => {
+    try {
+      await logOut();
+      // navigate('/signin')
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="w-[300] m-auto">
       <h1 className="text-center text-2xl font-bold pt-12">Account</h1>
@@ -13,7 +21,7 @@ const Account = () => {
         <p>Mail, {user?.email}</p>
         <p>Number, {user?.phoneNumber != null ? user?.phoneNumber : "No Number"}</p>
       </div>
-      <button className="border py-2 px-5 mt-10">Logout</button>
+      <button onClick={handleSignOut} className="border py-2 px-5 mt-10">Logout</button>
     </div>
   );
 };
