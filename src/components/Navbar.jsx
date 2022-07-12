8f717c4d729ec0a7665c6724656f8c10a502b56a
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-12 17:48:09 +0000

    added logout function and conditional rendering to navbar signin button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
+  const handleSignOut = async() => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className='flex justify-between bg-gray-200 w-full p-4'>
-      <h1 className='text-center text-2xl font-bold'>
+    <div className="flex justify-between bg-gray-200 w-full p-4">
+      <h1 className="text-center text-2xl font-bold">
         Firebase Google Auth & Content
       </h1>
-      <Link to='/signin'>Sign in</Link>
+      {user ? (
+        <button onClick={handleSignOut}>logout</button>
+      ) : (
+        <Link to="/signin">Sign in</Link>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
