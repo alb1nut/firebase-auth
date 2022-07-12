706032b7f04150e50125d3c5d53ddee8afe1d62b
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-12 17:17:07 +0000

    created handle google sign in  submit function

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -17,7 +17,7 @@ await googleSignIn();
     <div>
       <h1 className="text-center text-3xl font-bold py-8">Sign in</h1>
       <div className="max-w-[240px] m-auto py-4">
-        <GoogleButton onClick={handleGoogleSignIn}/>
+        <GoogleButton onClick={handleGoogleSignIn}/>          
       </div>
     </div>
   );
