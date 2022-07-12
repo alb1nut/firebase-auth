9a5b460b6cc92da38b59ab9a0d09ca707f4e674f
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-07-12 17:35:14 +0000

    created on user state change

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useEffect, useState } from "react";
 
 import {
     GoogleAuthProvider,
@@ -11,12 +11,22 @@ import { auth } from "../firebase";
 const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
+  const [user,setUser] =useState({})
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
   };
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth ,(currentUser)=>{
+      setUser(currentUser)
+    })
+    return () =>{
+      unsubscribe()
+    }
+  },[])
   return (
-    <AuthContext.Provider value={{googleSignIn}}>
+    <AuthContext.Provider value={{googleSignIn,user}}>
       {children}
     </AuthContext.Provider>
   );
